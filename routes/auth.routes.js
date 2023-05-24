e9ea77dacc8e10f35b72a39380723ddef412bc7a
syltur <sylwia.tur99@gmail.com>
2023-05-24 13:42:18 +0200

    init-project & add user 31.4

---
+++ routes/auth.routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+router.get(
+  '/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] })
+);
+
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
+  (req, res) => {
+    res.redirect('/user/logged');
+  }
+);
+
+router.post('/logout', (req, res, next) => {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
